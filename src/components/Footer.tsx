cd57904c8b1f0aefcbb844356a4af16953b07ed7
marcopolo1124 <85742840+marcopolo1124@users.noreply.github.com>
2023-02-07 10:54:10 +0000

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,68 @@
-import React from 'react'
+import Link from 'next/link'
+import React, {useState} from 'react'
 import { AiFillInstagram, AiOutlineTwitter, AiTwotoneMail} from 'react-icons/ai'
-import { FaDeviantart } from 'react-icons/fa'
+import { FaDeviantart, FaDiscord } from 'react-icons/fa'
 
 const Footer = () => {
+  const [name, setName] = useState<string>("")
+  const [email, setEmail] = useState<string>("")
+  const [message, setMessage] = useState<string>("")
+
   return (
     <div className='footer-container'>
       {/* <p>2023 JSM Headphones All rights reserved</p> */}
-      <p className='icons'>
-        <AiFillInstagram/>
-        <AiOutlineTwitter/>
-        <FaDeviantart/>
-        <AiTwotoneMail/>
+      <div className="footer-detail">
+        <div className='flex-container'>
+          <p>Use the form to contact me for enquiries and commissions. You can also find me on any of my social medias or by messaging me on Discord at Mochi#7921.</p>
+          <hr/>
+          <p className='icons'>
+            <Link href=""><AiFillInstagram/></Link>
+            <Link href=""><AiOutlineTwitter/></Link>
+            <Link href=""><FaDeviantart/></Link>
+            <Link href=""><FaDiscord/></Link>
+            <Link href=""><AiTwotoneMail/></Link>
 
-      </p>
+          </p>
+        </div> 
+      </div>
+      
+      <div className='contact-form'>
+        <form>
+            <input
+                name='name'
+                className="contact-input"
+                id='contact-name'
+                type={"text"}
+                onChange={(e) => {setName(e.target.value)}}
+                placeholder="Name"
+                value={name}
+                required
+          />
+          <input
+              name='email'
+              id='contact-email'
+              className="contact-input"
+              type={"email"}
+              onChange={(e) => {setEmail(e.target.value)}}
+              placeholder="Email"
+              value={email}
+              required
+          />
+          <textarea 
+            name='message'
+            placeholder="Message"
+            id='contact-message'
+            className="contact-input"
+            onChange={(e) => {setMessage(e.target.value)}}
+            required
+            value={message}
+            rows={7}
+          />
+
+          <input type={"submit"} value="SUBMIT"/>
+        </form>
+
+      </div>
     </div>
   )
 }
