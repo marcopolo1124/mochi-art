db7096b27bee947375431990e1e6767cfb5e8800
marcopolo1124 <85742840+marcopolo1124@users.noreply.github.com>
2023-02-10 23:29:24 +0000

    removed postadmin

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,10 +1,9 @@
-import { getAdminByUsername, postAdmin, updateAdminPassword, deleteAdmin} from "./admin";
+import { getAdminByUsername, updateAdminPassword, deleteAdmin} from "./admin";
 import {getState, getGallery, toggleArtTradeState, toggleCommissionState, postImage, deleteImage, getFeatured, getImage} from './site'
 import { getCommission, getCommissionsWithStatus, postCommission } from "./commissions";
 
 export default {
     getAdminByUsername,
-    postAdmin,
     updateAdminPassword,
     deleteAdmin,
     getState,
