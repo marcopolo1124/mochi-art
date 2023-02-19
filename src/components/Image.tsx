6d2939be32e990f56a1f8ff957ea54ad4cbd6e0e
marcopolo1124 <85742840+marcopolo1124@users.noreply.github.com>
2023-02-19 17:53:36 +0000

    debugging

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -4,6 +4,7 @@ import { Image as ImageType } from '@/types'
 import Link from 'next/link'
 const GalleryImage = ({title, file_name}: ImageType) => {
   const src = `${process.env.NEXT_PUBLIC_SERVER_URL}/static-gallery/${file_name}`
+  console.log(src)
   return (
     <div className="product-card">
       <Link href={`image/${file_name}`}>
