ca693a7f0dfc3364a632503ee45f5d224e4793ea
marcopolo1124 <85742840+marcopolo1124@users.noreply.github.com>
2023-02-19 15:41:53 +0000

    error handling

--- src/pages/admin/gallery.tsx
+++ src/pages/admin/gallery.tsx
@@ -1,7 +1,7 @@
-import { getFeatured, getImages, getUser } from '@/lib'
-import { GalleryImageForm, EditGallery } from '@/components'
+import { getFeatured, getImages } from '@/lib'
+import { EditGallery } from '@/components'
 import { Image } from '@/types'
-import React, {useEffect} from 'react'
+import React from 'react'
 import {RouteGuard} from '@/components'
 
 
@@ -21,11 +21,13 @@ const gallery = ({gallery}: {gallery: {images: Image[]}}) => {
 export default gallery
 
 export async function getServerSideProps() {
-    const featured = await getFeatured()
-    const gallery = await getImages("date", 1, 1000)
-    const userReq = await getUser()
-    const init = userReq.user? true: false
-    return {props: {featured, gallery, init}}
+    try{
+        const gallery = await getImages("date", 1, 1000)
+        return {props: {gallery}}
+    }catch(err){
+        return {props: {gallery: []}}
+    }
+
   
   }
   
