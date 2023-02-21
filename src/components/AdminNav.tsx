4ff47f8c0161c102999937fbcaf3cdfe0f33b997
marcopolo1124 <85742840+marcopolo1124@users.noreply.github.com>
2023-02-21 09:37:25 +0000

    change href

--- src/components/AdminNav.tsx
+++ src/components/AdminNav.tsx
@@ -7,7 +7,7 @@ const AdminNav = () => {
     <nav>
       <div className='navbar-container' id="admin-nav">
         <div className='navbar-menu' id="admin-nav-menu">
-            <Link href="/admin/state"><span className='admin-menu-item'>Site Settings</span></Link>
+            <Link href="/admin"><span className='admin-menu-item'>Site Settings</span></Link>
             <Link href="/admin/gallery"><span className='admin-menu-item'>Gallery Settings</span></Link>
             <Link href="/admin/commissions"><span className='admin-menu-item'>Commissions</span></Link>
             <div><span className='admin-menu-item' onClick={() => {signOut()}}>Logout</span></div>
